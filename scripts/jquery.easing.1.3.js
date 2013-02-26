453fbc8cfc5477b2b5712c71f91e53981a80e236
Jacob Farkas <jacobf@rkas.net>
2013-02-26 00:19:17 -0800

    More pages are wired up

--- scripts/jquery.easing.1.3.js
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>302 Found</title>
-</head><body>
-<h1>Found</h1>
-<p>The document has moved <a href="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js">here</a>.</p>
-</body></html>
